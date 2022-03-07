69414f5cccb395f528fee17a14161c126e114212
Sonia Mechaour <sonia.mechaour@hotmail.fr>
2022-03-07 16:38:17 +0100

    establish components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,38 @@
-import react, { Component} from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import react, { Component } from 'react';
+import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
 
-class  Header extends Component {
+class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
   }
-  render() { 
-    return (  
+  render() {
+    return (
       <div>
         <header>
-       
+          <Navbar bg="light" variant="light">
+            <Container>
+              <Navbar.Brand href="#home">Nook's cranny</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Accueil</Nav.Link>
+                <Nav.Link href="#features">Se connecter</Nav.Link>
+                <Nav.Link href="#pricing"></Nav.Link>
+              </Nav>
+            </Container>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar>
         </header>
       </div>
     );
   }
 }
- 
+
 export default Header;
