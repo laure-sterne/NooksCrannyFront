d54a4a5e22d68cbcdcc129c26a1a482c65b0425b
Camille <folcamille@yahoo.fr>
2022-03-07 16:02:44 +0100

    avancé et création des premiers composants

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import react, { Component} from 'react';
+import {Navbar, Nav, Container} from 'react-bootstrap';
+
+class  Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  
+      <div>
+        <header>
+       
+        </header>
+      </div>
+    );
+  }
+}
+ 
+export default Header;
