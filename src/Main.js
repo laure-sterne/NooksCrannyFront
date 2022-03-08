a66017ced0bdd4e9762c0613f93a6479d78820e8
Marchand-Matheo <91956913+Marchand-Matheo@users.noreply.github.com>
2022-03-08 15:11:18 +0100

    login.js

--- src/Main.js
+++ src/Main.js
@@ -31,7 +31,7 @@ class Main extends Component {
                         </Row>
                     </Container>
                 </div>
-                <Footer/ >
+                <Footer />  
             </div>
             
 
