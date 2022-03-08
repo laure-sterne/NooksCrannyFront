88b71ca32b678fba707b4e8885a2b83f20757180
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-03-08 11:01:00 +0100

    start adding components on homepage

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Components/Header'
+import Buttonbuy from './Components/buttonBuy';
 import Photos from './Components/carousel';
-import chair from './images/chair.jpg';
-
 
 class Main extends Component {
     constructor(props) {
@@ -12,8 +11,9 @@ class Main extends Component {
     render() { 
         return ( 
             <div>
-            <Header/ >
-            <Photos/ >
+            <Header />
+            <Photos />
+            <Buttonbuy />
             </div>
          );
     }
