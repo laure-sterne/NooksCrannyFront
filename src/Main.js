9ea2e448b8d466976e51ce26382467c393212bfd
YanissB <yaniss.besson@student.adatechschool.fr>
2022-03-08 11:42:58 +0100

    Added Cards and Layout

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './Components/Header'
 import Buttonbuy from './Components/buttonBuy';
 import Photos from './Components/carousel';
+import Cards from './Components/card';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class Main extends Component {
     constructor(props) {
@@ -14,9 +16,23 @@ class Main extends Component {
             <Header />
             <Photos />
             <Buttonbuy />
+                <div>
+                    <Container>
+                        <Row className="customRow">
+                            <Col><Cards /></Col>
+                            <Col><Cards /></Col>  
+                        </Row>
+                        <Row>
+                            <Col><Cards /></Col>
+                            <Col><Cards /></Col> 
+
+                        </Row>
+                    </Container>
+                </div>
             </div>
+            
          );
     }
 }
-
+ 
 export default Main;
