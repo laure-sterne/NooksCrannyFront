fd97e767a7216a6e19e692710f925b02de02efd2
Isciane <91957387+IscianeN@users.noreply.github.com>
2022-03-14 15:26:39 +0100

    fetch almost sucessfull

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
-import React, { Component, useEffect,useState} from 'react';
+import React, { Component } from 'react';
+import { useEffect, useState, setState } from 'react';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Buttonbuy from './Components/ButtonBuy';
@@ -7,37 +8,42 @@ import Cards from './Components/Card';
 //Import des composants de react bootstrap utilisés dans cette page (layout grid)
 import { Container, Row, Col } from 'react-bootstrap';
 
-
-
+        
 //page d'accueil
-class Main extends React.Component {
+class Main extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             error: null,
             isLoaded: false,
             meubles: []
-          };
+          }
+    };
+
+    componentDidMount() {
+    console.log("fetching") 
+    fetch("http://localhost:4000/meubles")
+      .then(res => res.json())
+      .then(
+        (res) => {
+          console.log(res)
+          this.setState({
+              isLoaded : true,
+              meubles : res
+            })
+          console.log("fetched")
+        })
     }
-    componentDidMount(){
-        fetch("http://localhost:4000/meubles")
-        .then(res => res.json())
-        .then(
-        (result) => {
-        console.log(result);
-        const meubles = result.data;
-        this.setState({isLoaded:true,meubles})
-        });
-    }
+
     render() {
-        console.log('bonjour')
         const { error, isLoaded, meubles } = this.state;
         if (error) {
             return <div>Erreur : {error.message}</div>;
-          } else if (!isLoaded) {
+          } else if (!isLoaded || !this.state.meubles[0]) {
             return <div>Chargement…</div>;
           } else {
-        return (
+        return ( 
             <div>
             {/* appel des composants de la page*/}
             <Header />
@@ -45,100 +51,24 @@ class Main extends React.Component {
                 <div>
                     <Container>
                         <Row className="customRow">
-                            <ul> { meubles.map(meuble => <li>{meuble}</li>)}</ul> 
+                            <p> il y a {meubles.length} meubles </p>
+                            <p> les meubles : {meubles.map(meuble => meuble.Nom)} </p>
                             <Col><Cards /></Col>
-                            <Col><Cards /></Col>
+                            <Col><Cards /></Col>  
                         </Row>
                         <Row>
                             <Col><Cards /></Col>
-                            <Col><Cards /></Col>
+                            <Col><Cards /></Col> 
+
                         </Row>
                     </Container>
                 </div>
-                <Footer />
+                <Footer />  
             </div>
          );
     }
 }
-
-
-
-
-
-
-
-//page d'accueil
-// class Main extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {  }
-//     }
-
-    //     fetch("http://localhost:4000/meubles")
-    //     .then(res => res.json())
-    //     .then(
-    //         (result) => {
-    //           this.setState({
-    //             isLoaded: true,
-    //             meubles: result.meubles
-    //           });
-    //         },
-    //         // Remarque : il est important de traiter les erreurs ici
-    //         // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
-    //         // des exceptions provenant de réels bugs du composant.
-    //         (error) => {
-    //           this.setState({
-    //             isLoaded: true,
-    //             error
-    //           });
-    //         }
-    //       )
-    //   }
-    
-    //   render() {
-    //     const { error, isLoaded, meubles } = this.state;
-    //     if (error) {
-    //       return <div>Erreur : {error.message}</div>;
-    //     } else if (!isLoaded) {
-    //       return <div>Chargement…</div>;
-    //     } else {
-    //       return (
-    //         <ul>
-    //           {meubles.map(meuble => (
-    //             <li key={meuble.nom}>
-    //               {meuble.nom}
-    //             </li>
-    //           ))}
-    //         </ul>
-    //       );
-    //     }
-    //   }
-    
-    // render() { 
-        
-       
-    //     return ( 
-    //         <div>
-    //         { /* appel des composants de la page*/}
-//             <Header />
-//             <CarouselMain />
-//                 <div>
-//                     <Container>
-//                         <Row className="customRow">
-//                             <Col><Cards /></Col>
-//                             <Col><Cards /></Col>  
-//                         </Row>
-//                         <Row>
-//                             <Col><Cards /></Col>
-//                             <Col><Cards /></Col> 
-
-//                         </Row>
-//                     </Container>
-//                 </div>
-//                 <Footer />  
-//             </div>
-//          );
-//     }
-// }
 }
+
+
 export default Main;
